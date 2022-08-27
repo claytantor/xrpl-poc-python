dcbac59a5ad1c80f08c50b016f0668306ac936bf
clay <claytantor@gmail.com>
2022-08-27 04:33:28 +0000

    working stop

--- devops/scripts/stop-compose.sh
+++ devops/scripts/stop-compose.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 export ENV=$1
-export DEVOPS_BASEDIR=$(pwd)
-docker-compose -f $DEVOPS_BASEDIR/docker/docker-compose.yml --env-file env/$ENV/xrpl-poc-python-app.env down
+export DEVOPS_BASEDIR=$(pwd)/devops
+docker-compose -f $DEVOPS_BASEDIR/compose/docker-compose.yml --env-file env/$ENV/xrpl-poc-python-app.env down
 docker image prune -a -f
