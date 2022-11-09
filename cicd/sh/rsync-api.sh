addf12868a2933c44658f5a419a04a4bd78cdc18
Clay Graham <claytantor@gmail.com>
2022-11-08 16:50:31 -0800

    sync of dev

---
+++ cicd/sh/rsync-api.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+rsync -Pav -e "ssh -i $HOME/data/keys/claytantor-oregon.pem" $HOME/data/github.com/claytantor/xrpl-poc-python/api ubuntu@ec2-34-211-56-213.us-west-2.compute.amazonaws.com:/home/ubuntu/xrpl-poc-python/. 
+rsync -Pav -e "ssh -i $HOME/data/keys/claytantor-oregon.pem" $HOME/data/github.com/claytantor/xrpl-poc-python/migrations ubuntu@ec2-34-211-56-213.us-west-2.compute.amazonaws.com:/home/ubuntu/xrpl-poc-python/. 
+rsync -Pav -e "ssh -i $HOME/data/keys/claytantor-oregon.pem" $HOME/data/github.com/claytantor/xrpl-poc-python/env ubuntu@ec2-34-211-56-213.us-west-2.compute.amazonaws.com:/home/ubuntu/xrpl-poc-python/. 
+rsync -Pav -e "ssh -i $HOME/data/keys/claytantor-oregon.pem" $HOME/data/github.com/claytantor/xrpl-poc-python/devops ubuntu@ec2-34-211-56-213.us-west-2.compute.amazonaws.com:/home/ubuntu/xrpl-poc-python/. 
