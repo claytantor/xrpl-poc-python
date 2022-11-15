d506dd707a6156cf2fb796244e6ce535d4a94962
Clay Graham <claytantor@gmail.com>
2022-11-14 20:37:22 -0800

    dont commit configs

--- cicd/sh/rsync-api.sh
+++ cicd/sh/rsync-api.sh
@@ -4,3 +4,4 @@ rsync -Pav -e "ssh -i $HOME/data/keys/claytantor-oregon.pem" $HOME/data/github.c
 rsync -Pav -e "ssh -i $HOME/data/keys/claytantor-oregon.pem" $HOME/data/github.com/claytantor/xrpl-poc-python/migrations ubuntu@ec2-34-211-56-213.us-west-2.compute.amazonaws.com:/home/ubuntu/xrpl-poc-python/. 
 rsync -Pav -e "ssh -i $HOME/data/keys/claytantor-oregon.pem" $HOME/data/github.com/claytantor/xrpl-poc-python/env ubuntu@ec2-34-211-56-213.us-west-2.compute.amazonaws.com:/home/ubuntu/xrpl-poc-python/. 
 rsync -Pav -e "ssh -i $HOME/data/keys/claytantor-oregon.pem" $HOME/data/github.com/claytantor/xrpl-poc-python/devops ubuntu@ec2-34-211-56-213.us-west-2.compute.amazonaws.com:/home/ubuntu/xrpl-poc-python/. 
+rsync -Pav -e "ssh -i $HOME/data/keys/claytantor-oregon.pem" $HOME/data/github.com/claytantor/xrpl-poc-python/requirements.txt ubuntu@ec2-34-211-56-213.us-west-2.compute.amazonaws.com:/home/ubuntu/xrpl-poc-python/. 
