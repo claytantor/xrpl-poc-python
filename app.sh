06b3b70f5d7064a2c81bf3439c52fa30401c7b1e
Clay Graham <claytantor@gmail.com>
2022-12-07 12:00:17 -0800

    fix app starter

--- app.sh
+++ app.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-# to do mobile testing and https use the certificate
-#FLASK_APP=api APP_CONFIG=env/local/xrpl-poc-python-app.env flask run  --host=0.0.0.0 --port=5000 --cert=cert.pem --key=key.pem --debugger --reload
 
-# no cert version
-# FLASK_APP=api APP_CONFIG=env/$1/xrpl-poc-python-app.env flask run  --host=0.0.0.0 --port=5000 --debugger --reload
+APP_CONFIG=env/$1/xrpl-poc-python-app.env python -m api
 
