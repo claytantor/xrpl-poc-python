ffa7995a77a17f99ffdc23de3c271a96c18d4556
Clay Graham <claytantor@gmail.com>
2022-12-08 18:30:57 -0800

    adding xumm callbacks

--- api/routes.py
+++ api/routes.py
@@ -477,7 +477,8 @@ def update_payment_item(
 def xumm_webhook(request: Request):
 
     ulogger.info(
-        f"==== xumm_webhook {request.method} {request.url} {request.headers} {request.body} {request.json()}")
+        f"==== xumm_webhook {request.method} {request.url} {request.headers} {request.body}")
+
 
 
 #     # ADHOC PAYMENT
